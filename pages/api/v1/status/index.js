839d2bce0018e45e19198175f665b87e7375e088
Allan Landin <allan.landin25@gmail.com>
2024-02-27 23:07:38 +0000

    Add tests about database.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,32 @@
 import database from "infra/database.js";
 
 async function status(req, res) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  res.status(200).json("teste");
+  const updateAt = new Date().toISOString();
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+
+  const openedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const oppenedConnections = openedConnectionsResult.rows[0].count;
+
+  const DbVersionResult = await database.query("SHOW server_version;");
+  const DbVersion = DbVersionResult.rows[0].server_version;
+
+  return res.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        version: DbVersion,
+        max_connections: parseInt(maxConnections),
+        opened_connections: oppenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,12 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+
+  expect(responseBody.update_at).toEqual(parsedUpdateAt);
+  expect(responseBody.dependecies.database.version).toBe("16.0");
+  expect(responseBody.dependecies.database.max_connections).toBeGreaterThan(0);
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1);
 });
