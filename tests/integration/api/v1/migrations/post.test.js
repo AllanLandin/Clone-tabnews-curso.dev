142c5c588e8adbbe3daf617e5061898ba14afcbc
Allan Landin <allan.landin25@gmail.com>
2024-11-09 18:09:36 +0000

    fix post to migration test

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -2,12 +2,14 @@ import database from "infra/database";
 import oschestrator from "../oschestrator";
 
 beforeAll(async () => {
-  await cleanDatabase();
   await oschestrator.waitForAllServices();
+  await cleanDatabase();
 });
 
 async function cleanDatabase() {
-  await database.query("drop schema public cascade; create schema public;");
+  await database.query(
+    "DROP SCHEMA public CASCADE; CREATE SCHEMA public; GRANT ALL ON SCHEMA public TO public;",
+  );
 }
 
 test("POST to /api/v1/migrations should return 200", async () => {
@@ -15,7 +17,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
     method: "POST",
   });
   const firstResponseBody = await firstResponse.json();
-  console.log(firstResponse.status);
   expect(firstResponse.status).toBe(201);
   expect(firstResponseBody.length).toBeGreaterThan(0);
 
@@ -23,7 +24,6 @@ test("POST to /api/v1/migrations should return 200", async () => {
     method: "POST",
   });
   const lastResponseBody = await lastResponse.json();
-  console.log(lastResponse.status);
   expect(lastResponse.status).toBe(200);
   expect(lastResponseBody.length).toBe(0);
 
